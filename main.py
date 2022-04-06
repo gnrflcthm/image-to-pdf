062c5e73fbeb31d184c1cf1a12e542159c146cfa
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-04-06 13:56:17 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from PIL import Image
+from pdfrw import PdfReader, PdfWriter
+import os
+import sys
+import shutil
+
+INPUT_DIR = "./input"
+TEMP_DIR = "./temp"
+OUTPUT_DIR = "./output"
+
+def convert_to_pdf():
+    input_files = [f'{INPUT_DIR}/{y}' for y in [x for x in os.walk(INPUT_DIR)][0][2]]
+    for f in input_files:
+        if f.startswith("."): continue
+        with Image.open(f) as img:
+            converted = img.convert("RGB")
+            file_name = f.split("/")[-1].split(".")[0]
+            converted.save(f"./{TEMP_DIR}/{file_name}.pdf")
+
+def merge_pdf(filename):
+    input_files = [f'{TEMP_DIR}/{y}' for y in [x for x in os.walk(TEMP_DIR)][0][2]]
+    writer = PdfWriter(fname=f"{OUTPUT_DIR}/{filename}")
+    print(input_files)
+    for f in input_files:
+        pdf = PdfReader(f)
+        writer.addPage(pdf.pages[0])
+    writer.write()
+
+def generate_pdf():
+    filename = ""
+    if len(sys.argv) == 1:
+        filename = "output"
+    else:
+        filename = sys.argv[1]
+    convert_to_pdf()
+    merge_pdf(filename)
+    clear_temp()
+
+def clear_temp():
+    input_files = [f'{TEMP_DIR}/{y}' for y in [x for x in os.walk(TEMP_DIR)][0][2]]
+    for f in input_files:
+        if f.startswith(".blank"):
+            continue
+        os.remove(f)
+    pass
+
+generate_pdf()
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Usage
+
+### 1. Add images to input folder.
+
+### 2. Open command prompt then run the following command.
+
+    `python main.py <filename>`
+
+### Where \<filename> is the name of the output file.
+
+### 3. Check the output folder for the generated pdf.
+
+Notes:
+- order of the files to pages is based on system default
+- if you want to specify the order of the files in the generated pdf, 
+name the files numerically (eg. 1.jpeg, 2.jpeg, etc.) 
